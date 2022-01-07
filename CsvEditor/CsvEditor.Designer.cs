da6e9ecb65f7957c4d3df286fb6268280f2cd023
Van Eester Michiel <michielvaneester@gmail.com>
2022-01-07 10:35:45 +0100

    Initial commit

---
+++ CsvEditor/CsvEditor.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace CsvEditor
+{
+    partial class CsvEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblCsvFile = new System.Windows.Forms.Label();
+            this.txtCsvFile = new System.Windows.Forms.TextBox();
+            this.btnImportFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(768, 571);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblCsvFile
+            // 
+            this.lblCsvFile.AutoSize = true;
+            this.lblCsvFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCsvFile.Location = new System.Drawing.Point(10, 15);
+            this.lblCsvFile.Name = "lblCsvFile";
+            this.lblCsvFile.Size = new System.Drawing.Size(57, 13);
+            this.lblCsvFile.TabIndex = 1;
+            this.lblCsvFile.Text = "Bestand:";
+            // 
+            // txtCsvFile
+            // 
+            this.txtCsvFile.Location = new System.Drawing.Point(76, 12);
+            this.txtCsvFile.Name = "txtCsvFile";
+            this.txtCsvFile.Size = new System.Drawing.Size(205, 20);
+            this.txtCsvFile.TabIndex = 2;
+            // 
+            // btnImportFile
+            // 
+            this.btnImportFile.Location = new System.Drawing.Point(287, 10);
+            this.btnImportFile.Name = "btnImportFile";
+            this.btnImportFile.Size = new System.Drawing.Size(63, 23);
+            this.btnImportFile.TabIndex = 3;
+            this.btnImportFile.Text = "Importeer";
+            this.btnImportFile.UseVisualStyleBackColor = true;
+            // 
+            // CsvEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 621);
+            this.Controls.Add(this.btnImportFile);
+            this.Controls.Add(this.txtCsvFile);
+            this.Controls.Add(this.lblCsvFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CsvEditor";
+            this.Text = "CSV Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCsvFile;
+        private System.Windows.Forms.TextBox txtCsvFile;
+        private System.Windows.Forms.Button btnImportFile;
+    }
+}
+
