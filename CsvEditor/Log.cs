f791c637ef950b96397277dfcbe84ccf65deba31
Van Eester Michiel <michielvaneester@gmail.com>
2022-01-10 14:00:49 +0100

    Log fixed

--- CsvEditor/Log.cs
+++ CsvEditor/Log.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CsvEditor
 {
     class Log
     {
-        //TODO
+        public static string dir = Application.StartupPath;
+        public static string path = Path.Combine(dir, "log.txt");
+
+        public static void Write(string msg)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = File.AppendText(path);
+                sw.WriteLine($"{} :: {msg}");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
     }
 }
